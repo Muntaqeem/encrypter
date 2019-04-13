7f7d4390d64f959589b8c650bbbda93078dac84a
muktadir <triplemzim@gmail.com>
2019-04-14 05:22:34 +0800

    finished basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,32 +1,158 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
+import { Button, LinearProgress } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const inputStyle = {
+  display: 'none'
+}
+
+let progressVal = 0;
+let lastProgress = 0;
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      filepath: '',
-      password: ''
+      fileName: '',
+      password: '',
+      progressState: 0
+    }
+    this.fileinputRef = React.createRef();
+    window.setInterval(()=>{
+      this.checkProgress();
+    },250);
+  }
+
+  checkProgress = () => {
+    if(progressVal !== lastProgress){
+      lastProgress = progressVal;
+      this.setState({
+        progressState: lastProgress
+      });
     }
   }
 
   getButtonState = () => {
-    if ( this.state.filepath === '') return true;
+    if ( this.state.fileName === '') return true;
     return false;
   }
+
+  onClickFileSelect = () => {
+   this.fileinputRef.current.click();
+  }
+
+  fileSelected = () => {
+    progressVal = 0;
+    debugger;
+    let fullPath = this.fileinputRef.current.value;
+    if(fullPath === undefined) alert("Empty File! Please try again.");
+    let startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+    let filename = fullPath.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+        filename = filename.substring(1);
+    }
+    this.setState({
+      fileName: filename,
+      progressState: 0
+    });
+  }
+
+  encrypt = () => {
+    progressVal = 0;
+
+    let password = prompt("Please enter secret key!", "");
+    let key = 1;
+    for(let i = 0; i < password.length; i++){
+      key = key + password.charCodeAt(i); 
+    }
+    while(key < 500) {
+      key = key + key;
+    }
+
+    // alert(this.fileinputRef.current.files[0].size);
+    let reader = new FileReader();
+    let useFile = this.fileinputRef.current.files[0];
+    reader.readAsArrayBuffer(useFile);
+    
+    reader.onload = (e) => {
+      let byteArray = new Uint8Array(reader.result);
+
+      byteArray.reverse();
+      let k;
+
+      for(let i = key; i < byteArray.length; i += key){
+        k = byteArray[i];
+        byteArray[i] = byteArray[i-key];
+        byteArray[i-key] = k; 
+        progressVal = parseInt((i / byteArray.length) * 100);       
+      }
+
+      let output = new Blob([byteArray], {type: 'video/zim'});
+      window.open(URL.createObjectURL(output), 'one.mkv');
+    }
+
+    // reader.onprogress = (data) => {
+    //   if(data.lengthComputable){
+    //     progressVal = parseInt(((data.loaded/data.length) * 100), 10)
+    //   }
+    // }
+
+  }
+
+  decrypt = () => {
+    progressVal = 0;
+
+    let password = prompt("Please enter secret key!", "");
+    let key = 1;
+    for(let i = 0; i < password.length; i++){
+      key = key + password.charCodeAt(i); 
+    }
+    while(key < 500) {
+      key = key + key;
+    }
+
+    let reader = new FileReader();
+    let useFile = this.fileinputRef.current.files[0];
+    reader.readAsArrayBuffer(useFile);
+    
+    reader.onload = (e) => {
+      let byteArray = new Uint8Array(reader.result);
+
+      let k;
+
+      for(let i = byteArray.length - (byteArray.length%key); i >= 0; i -= key){
+        k = byteArray[i];
+        byteArray[i] = byteArray[i-key];
+        byteArray[i-key] = k;  
+        progressVal = parseInt(((byteArray.length - i) / byteArray.length ) * 100);      
+      }
+      byteArray.reverse();
+
+      let output = new Blob([byteArray], {type: 'video/mkv'});
+      window.open(URL.createObjectURL(output), 'one.mkv');
+    }
+
+  }
+
+
+
+
+
   render() {
     return (
       <div className="App">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        <p class="title">Offline Encrypter: Encrypt/Decrypt files with password!</p>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <TextField
             disabled
             id="filePath"
-            value={this.state.filepath}
+            value={this.state.fileName}
             placeholder="File Path"
             margin="normal"
           />
@@ -35,9 +161,14 @@ class App extends Component {
             variant="outlined"
             color="primary"
             className="button-common"
+            onClick={this.onClickFileSelect}
             >
             Select File
             </Button>
+          </div>
+          <input id="file-input" ref={this.fileinputRef} type="file" style={inputStyle} onChange={this.fileSelected}/>
+          <div className="progressbar">
+            <LinearProgress color="primary" variant="determinate" value={this.state.progressState } />
           </div>
           <div>
             
@@ -46,6 +177,7 @@ class App extends Component {
                 variant="outlined"
                 color="secondary" 
                 className="button-common"
+                onClick={this.encrypt}
                 disabled={this.getButtonState()}
               >
                 Encrypt File
@@ -56,21 +188,32 @@ class App extends Component {
                 variant="outlined"
                 color="secondary" 
                 className="button-common"
+                onClick={this.decrypt}
                 disabled={this.getButtonState()}
               >
                 Decrypt File
               </Button>
             </div>
           </div>
+          
           <a
             className="App-link"
-            href="http://github.com/triplemzim"
+            href="http://github.com/triplemzim/encrypter"
             target="_blank"
             rel="noopener noreferrer"
           >
             Project link
           </a>
         </header>
+        <footer>
+          <a
+          className="Footer"
+          href="https://www.linkedin.com/in/md-muhim-muktadir-khan-997956115"
+          target="_blank"
+          rel="noopener noreferrer">
+            Fun Tool by Md Muhim Muktadir Khan
+          </a>          
+        </footer>
       </div>
     );
   }
