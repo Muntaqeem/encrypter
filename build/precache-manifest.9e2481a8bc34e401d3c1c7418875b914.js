e0b799b8229d5b11c6271864b24bb6a712cffb53
muktadir <triplemzim@gmail.com>
2019-04-14 00:42:46 +0800

    production build

---
+++ build/precache-manifest.9e2481a8bc34e401d3c1c7418875b914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7912c18dc706bebe7d73",
+    "url": "/static/js/main.6bd26161.chunk.js"
+  },
+  {
+    "revision": "ef03b6043e6b158d8e2d",
+    "url": "/static/js/2.995d95a2.chunk.js"
+  },
+  {
+    "revision": "7912c18dc706bebe7d73",
+    "url": "/static/css/main.fa54ced4.chunk.css"
+  },
+  {
+    "revision": "210195eea4fca5289921c0729a1b6391",
+    "url": "/index.html"
+  }
+];
